f17be1118d15a1f2091288e142f871d9ffc2f31a
jace chen <18191459044@163.com>
2019-12-31 11:26:18 +0800

    清时明月汉时光，万里长征人未还

--- MasonryTest/ViewController.m
+++ MasonryTest/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 #import "Masonry.h"
 @interface ViewController ()
+@property (nonatomic,strong)UIView *redView;
+@property (nonatomic,strong)UIView *yellowView;
+@property (nonatomic,strong)UIView *blueView;
 
 @end
 
@@ -23,17 +26,10 @@
     
 #pragma mark 3. 使用Masonry时进行动画操作方法)
     [self demo_three];
-
-    
 }
 
-
-
 -(void)demo_three{
-    UIView *redView = [[UIView alloc] init];
-    redView.backgroundColor = [UIColor redColor];
-    [self.view addSubview:redView];
-    [redView mas_makeConstraints:^(MASConstraintMaker *make) {
+    [self.redView mas_makeConstraints:^(MASConstraintMaker *make) {
         make.size.mas_equalTo(CGSizeMake(200,200));
         make.center.mas_equalTo(self.view);
     }];
@@ -50,19 +46,16 @@
                  make.centerY.mas_equalTo(self.view).offset(pad);
                  yellowView.backgroundColor = [UIColor redColor];
 
-                
                 return ;
             }else if(i == 1){
                 make.centerX.mas_equalTo(self.view).offset(-pad);
                 make.centerY.mas_equalTo(self.view).offset(pad);
                 yellowView.backgroundColor = [UIColor blueColor];
 
-
             }else if(i == 2){
                 make.centerX.mas_equalTo(self.view).offset(pad);
                 make.centerY.mas_equalTo(self.view).offset(-pad);
                 yellowView.backgroundColor = [UIColor yellowColor];
-
 
             }else if(i == 3){
                 make.centerX.mas_equalTo(self.view).offset(-pad);
@@ -74,15 +67,9 @@
     }
 }
 
-
-
-
 #pragma mark 2. 使用Masonry时进行动画操作方法)
 -(void)demo_tow{
-    UIView *redView = [[UIView alloc] init];
-    redView.backgroundColor = [UIColor redColor];
-    [self.view addSubview:redView];
-    [redView mas_makeConstraints:^(MASConstraintMaker *make) {
+    [self.redView mas_makeConstraints:^(MASConstraintMaker *make) {
         make.size.mas_equalTo(CGSizeMake(10,10));
         make.center.mas_equalTo(self.view);
     }];
@@ -91,47 +78,48 @@
         //告知需要更改约束
         [self.view setNeedsUpdateConstraints];
         [UIView animateWithDuration:2 animations:^{
-            [redView mas_updateConstraints:^(MASConstraintMaker *make) {
+            [self.redView mas_updateConstraints:^(MASConstraintMaker *make) {
                 make.size.mas_equalTo(CGSizeMake(250, 250));
               }];
            //告知父类控件绘制，不添加注释的这两行的代码无法生效
-          [redView.superview layoutIfNeeded];
+          [self.redView.superview layoutIfNeeded];
         }];
-
     });
 }
 
-
-
-
 #pragma mark 1. 三个View等宽_等高
 -(void)demo_one{
-    
-    UIView *redView = [[UIView alloc] init];
-    redView.backgroundColor = [UIColor redColor];
-    [self.view addSubview:redView];
-    [redView mas_makeConstraints:^(MASConstraintMaker *make) {
+    [self.redView mas_makeConstraints:^(MASConstraintMaker *make) {
         make.size.mas_equalTo(CGSizeMake(CGRectGetWidth(self.view.frame)/3, CGRectGetHeight(self.view.frame)));
-//        make.left.top.mas_equalTo(0);
     }];
-
-    UIView *yellowView =  [[UIView alloc] init];
-    yellowView.backgroundColor = [UIColor yellowColor];
-    [self.view addSubview:yellowView];
-    [yellowView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.size.top.mas_equalTo(redView);
-        make.left.mas_equalTo(redView.mas_right);
+    [self.yellowView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.size.top.mas_equalTo(self.redView);
+        make.left.mas_equalTo(self.redView.mas_right);
     }];
-    
-    
-    UIView *blueView =  [[UIView alloc] init];
-    blueView.backgroundColor = [UIColor blueColor];
-    [self.view addSubview:blueView];
-    [blueView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.size.top.mas_equalTo(redView);
-        make.left.mas_equalTo(yellowView.mas_right);
+    [self.blueView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.size.top.mas_equalTo(self.redView);
+        make.left.mas_equalTo(self.yellowView.mas_right);
     }];
 }
 
+-(UIView *)redView{
+    if (!_redView) {
+        _redView.backgroundColor = [UIColor redColor];
+        [self.view addSubview:_redView];
+    }return _redView;
+}
 
+-(UIView *)yellowView{
+    if (!_yellowView) {
+        _yellowView.backgroundColor = [UIColor yellowColor];
+        [self.view addSubview:_yellowView];
+    }return _yellowView;
+}
+
+-(UIView *)blueView{
+    if (!_blueView) {
+        _blueView.backgroundColor = [UIColor blueColor];
+        [self.view addSubview:_blueView];
+    }return _blueView;
+}
 @end
